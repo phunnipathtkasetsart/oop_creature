65fb66e57d5c70518af0d2a8ee908f2f29d17ade
phunnipath.t@ku.th <phunnipath.t@ku.th>
2025-11-19 09:44:22 +0700

    Add SwimmingCreature

--- creature_simulation.py
+++ creature_simulation.py
@@ -17,6 +17,30 @@ class Creature:
 
     def __str__(self):
         return f"{self.name} (HP: {self.hp})"
+# ===============================
+# SwimmingCreature Branch
+# ===============================
+
+class SwimmingCreature(Creature):
+    """Your code here"""
+    def __init__(self, name, hp, attack_power,depth = 0):
+        super().__init__(name, hp, attack_power,)
+        self.depth = depth
+    def dive_to(self,new_depth):
+        self.depth = new_depth
+        print(f"{self.name} dives to depth {self.depth} meters")
+        
+    def attack(self, target):
+        
+        if not self.is_alive():
+            print(f"{self.name} cannot attack because it is defeated.")
+            return
+        
+        print(f"{self.name} attacks from underwater at depth {self.depth}!")
+        print(f"It splashes {target.name} for {self.attack_power} damage!")
+
+        target.hp -= self.attack_power 
+        print(f"{target.name} HP is now {target.hp}")
 
 if __name__ == "__main__":
     print("=== Creature Class Tests ===\n")
@@ -66,5 +90,16 @@ if __name__ == "__main__":
     goblin.attack(slime)
     print(f"Slime should be at HP 0 → Actual: {slime.hp}")
     print()
+    print("=== Tests Completed ===")
+    print()
 
+    print("=== SwimmingCreature Tests ===\n")
+    serpent = SwimmingCreature("Aqua Serpent", 60, 7)
+    serpent.dive_to(30)
+    print(f"Depth should be 30 → Actual: {serpent.depth}")
+
+    dummy = Creature("Practice Dummy", 40, 0)
+    serpent.attack(dummy)
+    print(f"Dummy HP should be 33 → Actual: {dummy.hp}")
+    print()
     print("=== Tests Completed ===")
